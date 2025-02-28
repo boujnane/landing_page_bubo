2e9d373a28fab904dd6ace7f5646d975cc852d4d
ady boujnane <ady.boujnane@gmail.com>
2025-03-01 00:44:42 +0100

    Fix: rebase and changes

--- src/config/products.ts
+++ src/config/products.ts
@@ -4,31 +4,31 @@ export const products = [
     description: "biscuits.a_delicious_cocoa_cookie",
     price: 2.20,
     image: "/images/products/fb_cacao-nobg.png",
-    ingredients: ["biscuits.potato_flour", "biscuits.wheat_flour", "biscuits.milk", "biscuits.sugar", "biscuits.water", "biscuits.butter", "biscuits.salt", "biscuits.eggs", "biscuits.chocolate", "biscuits.cocoa"],
-    weight: "50 g"
+    ingredients: ["biscuits.sweet_potato_flour", "biscuits.wheat_flour", "biscuits.milk", "biscuits.sugar", "biscuits.water", "biscuits.butter", "biscuits.salt", "biscuits.eggs", "biscuits.chocolate", "biscuits.cocoa"],
+    weight: "75 g"
   },
   {
     name: "biscuits.garlic_Chilli_flavour",
     description: "biscuits.a_spicy_touch",
     price: 2.20,
     image: "/images/products/fb_piment-nobg.png",
-    ingredients: ["biscuits.potato_flour", "biscuits.wheat_flour", "biscuits.milk", "biscuits.sugar", "biscuits.water", "biscuits.butter", "biscuits.salt", "biscuits.eggs", "biscuits.garlic", "biscuits.pepper"],
-    weight: "50 g"
+    ingredients: ["biscuits.sweet_potato_flour", "biscuits.wheat_flour", "biscuits.milk", "biscuits.sugar", "biscuits.water", "biscuits.butter", "biscuits.salt", "biscuits.eggs", "biscuits.garlic", "biscuits.pepper"],
+    weight: "75 g"
   },
   {
     name: "biscuits.sugar_free_flavor",
     description: "biscuits.a_simple_and_tasty",
     price: 2.20,
     image: "/images/products/fb_sanssucre-nobg.png",
-    ingredients: ["biscuits.potato_flour", "biscuits.wheat_flour", "biscuits.milk", "biscuits.water", "biscuits.butter", "biscuits.salt", "biscuits.eggs"],
-    weight: "50 g"
+    ingredients: ["biscuits.sweet_potato_flour", "biscuits.wheat_flour", "biscuits.milk", "biscuits.water", "biscuits.butter", "biscuits.salt", "biscuits.eggs"],
+    weight: "75 g"
   },
   {
     name: "biscuits.coconut_flavor",
     description: "biscuits.an_exotic_coconut",
     price: 2.20,
     image: "/images/products/fb_coco-nobg.png",
-    ingredients: ["biscuits.potato_flour", "biscuits.wheat_flour", "biscuits.milk", "biscuits.sugar", "biscuits.water", "biscuits.butter", "biscuits.salt", "biscuits.eggs", "biscuits.coconut"],
-    weight: "50 g"
+    ingredients: ["biscuits.sweet_potato_flour", "biscuits.wheat_flour", "biscuits.milk", "biscuits.sugar", "biscuits.water", "biscuits.butter", "biscuits.salt", "biscuits.eggs", "biscuits.coconut"],
+    weight: "75 g"
   }
 ];
